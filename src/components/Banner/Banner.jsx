d5d118d513379510d84bfb1d9fb1b731061b07ae
Anik Islam <anik54908@gmail.com>
2025-11-08 00:51:51 +0600

    added some classes

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -29,8 +29,8 @@ const Banner = () => {
                             {/* slick items */}
                             <Slider {...settings}>
                                 <div className="slider-item font-inter mb-5">
-                                    <h1 className='text-white text-[35px] lg:text-[75px] font-bold leading-[120%] font-inter '>We Help brands with high quality services</h1>
-                                    <p className='text-[16px] text-white pt-[50px] pb-10 max-w-[620px] mx-auto px-4 lg:px-0'>There are many variations of passages of Lorem Ipsum available, but the majority  have suffered alteration in some form, by injected humour, or randomised words which don't look.</p>
+                                    <h1 className='text-white text-[35px] sm:[30px] sm:pb-2.5 lg:text-[75px] font-bold leading-[120%] font-inter '>We Help brands with high quality services</h1>
+                                    <p className='text-[16px] text-white sm:pt-[30PX] pt-[50px] pb-10 max-w-[620px] mx-auto px-4 lg:px-0'>There are many variations of passages of Lorem Ipsum available, but the majority  have suffered alteration in some form, by injected humour, or randomised words which don't look.</p>
                                     <a href="#" className="py-[15px] px-[42px] bg-[#6A4DF4] text-white text-[20px] rounded-[10px] font-semibold border-2 border-[#6A4DF4] hover:bg-transparent  duration-300 font-inter">Get Started</a>
                                 </div>
 
