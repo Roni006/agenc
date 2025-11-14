e3c56da5e368dc526cd9399b584713aa5219e8ac
Anik Islam <anik54908@gmail.com>
2025-11-14 20:35:44 +0600

    resolve the file problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Demo from './components/demo/demo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
